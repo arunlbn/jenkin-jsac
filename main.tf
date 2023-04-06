27206ccb67eb0d0c3a4d55b631bc191867d1f209
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 14:55:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -240,19 +240,42 @@ resource "aws_lb_target_group" "jenkins_tg" {
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
 }
+  
+data "aws_acm_certificate" "amazon_issued" {
+  domain      = "lbncyberlabs.com"
+  types       = ["AMAZON_ISSUED"]
+  most_recent = true
+}  
 
-resource "aws_lb_listener" "jenkins_end" {
+resource "aws_lb_listener" "jenkins_frontend" {
   load_balancer_arn = aws_lb.jenkins_alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  certificate_arn   = data.aws_acm_certificate.arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.jenkins_tg.arn
   }
 }  
+
+data "aws_route53_zone" "lbn" {
+  name         = "lbncyberlabs.com."
+  private_zone = false
+}
   
+  
+resource "aws_route53_record" "jenkins" {
+  zone_id = data.aws_route53_zone.lbn.zone_id
+  name    = "ci.lbncyberlabs.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.jenkins_alb.dns_name
+    zone_id                = aws_lb.jenkins_alb.zone_id
+    evaluate_target_health = false
+  }
+}
 
 
