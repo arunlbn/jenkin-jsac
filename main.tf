f703b4c9db2d62b3d12f2d41a781f74ee08c650d
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 19:38:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "jenkins_service_sg" {
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
-      cidr_blocks = [module.vpc.vpc_cidr_block]
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
   dynamic "egress" {
