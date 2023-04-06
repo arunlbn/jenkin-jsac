eb7adc7c477814d2e1a97d7970a76851f252735f
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 09:47:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,24 +23,24 @@ resource "aws_security_group" "jenkins_service_sg" {
   description = "Security group for jenkins-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = module.vpc.vpc_id
 
-  ingress [{
+  ingress {
     description      = "SSH from anywhere"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   },
-    
+  {  
     description      = "Jenkins from anywhere"
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }, 
+  } 
     
 
     
-  ]
+  
   egress {
     from_port        = 0
     to_port          = 0
