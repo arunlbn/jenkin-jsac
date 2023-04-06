9671b518153435a7236c110096bd410e3ebfe806
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 19:03:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -200,6 +200,19 @@ resource "aws_launch_template" "jenkins_lt" {
   }
 
  vpc_security_group_ids = [aws_security_group.jenkins_service_sg.id]
+  
+ user_data = "${base64encode(<<EOF
+  #!/bin/bash
+  apt update -y
+  apt upgrade -y
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo ap update -y
+  sudo apt install openjdk-11-jre -y
+  sudo apt install jenkins -y
+  sudo systemctl enable jenkins
+EOF
+)}"
 
   tag_specifications {
     resource_type = "instance"
