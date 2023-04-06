400effd8bdb7cc8bdc7376eb31e694c5489b04c3
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 14:57:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -252,7 +252,7 @@ resource "aws_lb_listener" "jenkins_frontend" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.arn
+  certificate_arn   = data.aws_acm_certificate.amazon_issued.arn
 
   default_action {
     type             = "forward"
