b07eac0f75c2fd4080a7bc576adacdc3003ab1cc
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 14:19:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "jenkins_service_sg" {
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = [module.vpc.vpc_cidr_block]
     }
   }
   dynamic "egress" {
@@ -51,7 +51,7 @@ resource "aws_security_group" "jenkins_service_sg" {
       from_port   = egress.value
       to_port     = egress.value
       protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = [module.vpc.vpc_cidr_block]
     }
   }
 
@@ -75,16 +75,14 @@ resource "aws_security_group" "efs_sg" {
       security_groups = [aws_security_group.jenkins_service_sg.id]
     }
   
-  dynamic "egress" {
-    for_each = toset(local.ports_out)
-    content {
+  egress {
       description = "All traffic to EC2"
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
       security_groups = [aws_security_group.jenkins_service_sg.id]
     }
-  }
+  
 
   tags = {
     Terraform   = "true"
@@ -93,6 +91,26 @@ resource "aws_security_group" "efs_sg" {
   }  
 }
   
+resource "aws_security_group" "jenkins_alb_sg" {
+  name        = "jenkins-alb"
+  description = "Security group for jenkins-alb with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = module.vpc.vpc_id
+
+   ingress {
+      description = "Traffic from internet"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      security_groups = ["0.0.0.0/0"]
+    }
+  
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    service = "jenkins"
+  }  
+}
   
 resource "aws_efs_file_system" "jenkins_efs" {
   creation_token = "jenkins-efs"
@@ -188,7 +206,7 @@ resource "aws_launch_template" "jenkins_lt" {
   
 }
   
-resource "aws_autoscaling_group" "jenkins-asg" {
+resource "aws_autoscaling_group" "jenkins_asg" {
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   desired_capacity   = 1
   max_size           = 1
@@ -199,4 +217,23 @@ resource "aws_autoscaling_group" "jenkins-asg" {
     version = "$Latest"
   }
 }
+  
+resource "aws_lb" "jenkins_lab" {
+  name               = "jenkins-lab"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [module.vpc.public_subnets]
+
+  enable_deletion_protection = false
+
+  
+  tags = {
+      Name = "jenkins-lt"
+      Terraform   = "true"
+      Environment = "dev"
+      service = "jenkins"
+  }
+}  
+
 
