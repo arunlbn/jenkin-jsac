f97dc19d2b497cf154339e7ebbf7c503a5de14d0
arunlbn <97378356+arunlbn@users.noreply.github.com>
2023-04-06 19:17:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -205,8 +205,11 @@ resource "aws_launch_template" "jenkins_lt" {
   #!/bin/bash
   apt update -y
   apt upgrade -y
-  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | \
+         sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]  \
+       https://pkg.jenkins.io/debian-stable binary/ | sudo tee  \
+       /etc/apt/sources.list.d/jenkins.list > /dev/null
   sudo ap update -y
   sudo apt install openjdk-11-jre -y
   sudo apt install jenkins -y
